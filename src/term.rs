266b1bbb3b72f484a0d03aa40cb0bf71aa89cfbc
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-08-22 20:46:29 +0900

    TOTAL REWRITE

--- src/term.rs
+++ src/term.rs
@@ -1,144 +1,104 @@
 use enum_variant_type::EnumVariantType;
 use maplit::hashset;
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt,
+};
 
-// TODO: 2023/07/06 Dummyどうしの比較は常にNot Eqになってほしい
 #[derive(Clone, Debug, EnumVariantType, Eq, Hash, PartialEq)]
 pub enum Term {
-    Var(VarID),
-    UnificationTerm(UnificationTermID),
-    Function(String, Vec<Term>),
-    Dummy,
+    Var(usize),
+    Function(usize, Vec<Term>),
 }
 
-type VarID = String;
-type UnificationTermID = (usize, usize);
-
 impl Term {
-    pub fn get_free_vars(&self) -> HashSet<&VarID> {
-        let mut free_vars = HashSet::new();
+    fn free_vars(&self) -> HashSet<usize> {
+        let mut vars = hashset!();
+        self.free_vars_rec(&mut vars);
+        vars
+    }
+
+    fn free_vars_rec(&self, vars: &mut HashSet<usize>) {
         use Term::*;
         match self {
             Var(id) => {
-                free_vars.insert(id);
-            }
-            UnificationTerm(_) | Dummy => {}
-            Function(_, terms) => {
-                for term in terms {
-                    free_vars.extend(term.get_free_vars())
-                }
-            }
-        }
-        free_vars
-    }
-
-    pub fn get_free_vars2(&self) -> HashSet<&VarID> {
-        use Term::*;
-        match self {
-            Var(id) => hashset!(id),
-            UnificationTerm(_) | Dummy => hashset!(),
-            Function(_, terms) => terms
-                .iter()
-                .flat_map(|term| term.get_free_vars2())
-                .collect(),
-        }
-    }
-
-    fn get_unification_terms(&self) -> HashSet<&UnificationTermID> {
-        let mut unification_terms = HashSet::new();
-        use Term::*;
-        match self {
-            Var(_) | Dummy => {}
-            UnificationTerm(id) => {
-                unification_terms.insert(id);
+                vars.insert(*id);
             }
             Function(_, terms) => {
                 for term in terms {
-                    unification_terms.extend(term.get_unification_terms())
+                    term.free_vars_rec(vars);
                 }
             }
         }
-        unification_terms
     }
 
     // TODO: 2023/07/06 所有権をどうするか
-    // TODO: 2023/07/06 VarIDかVarか
-    pub fn replace_var(self, var_id: &VarID, new_term: &Self) -> Self {
+    // TODO: 2023/08/22 &mut selfとの比較
+    // TODO: 2023/08/22 そもそも必要か：substitutionだけでよいのでは
+    // TODO: 2023/08/22 subst_varの方がいい？
+    fn replace_var(self, var: usize, new_term: &Self) -> Self {
         use Term::*;
         match self {
             Var(id) => {
-                if &id == var_id {
+                if id == var {
                     new_term.clone()
                 } else {
                     Var(id)
                 }
             }
-            UnificationTerm(_) | Dummy => self,
             Function(id, terms) => {
                 let terms = terms
                     .into_iter()
-                    .map(|term| term.replace_var(var_id, new_term))
+                    .map(|term| term.replace_var(var, new_term))
                     .collect();
                 Function(id, terms)
             }
         }
     }
 
-    pub fn replace_unification_term(
-        self,
-        unification_term_id: &UnificationTermID,
-        new_term: &Self,
-    ) -> Self {
+    fn replace_var2(&mut self, var: usize, new_term: &Self) {
         use Term::*;
         match self {
-            Var(_) | Dummy => self,
-            UnificationTerm(id) => {
-                if &id == unification_term_id {
-                    new_term.clone()
-                } else {
-                    UnificationTerm(id)
+            Var(id) => {
+                if *id == var {
+                    *self = new_term.clone();
                 }
             }
-            Function(id, terms) => {
-                let terms = terms
-                    .into_iter()
-                    .map(|term| term.replace_unification_term(unification_term_id, new_term))
-                    .collect();
-                Function(id, terms)
+            Function(_, terms) => {
+                for term in terms {
+                    term.replace_var2(var, new_term);
+                }
             }
         }
     }
 
-    pub fn replace_unification_terms(self, map: &HashMap<UnificationTermID, Self>) -> Self {
+    fn replace_vars(self, map: &HashMap<usize, Self>) -> Self {
         use Term::*;
         match self {
-            Var(_) | Dummy => self,
-            UnificationTerm(id) => {
+            Var(id) => {
                 if let Some(new_term) = map.get(&id) {
                     new_term.clone()
                 } else {
-                    UnificationTerm(id)
+                    Var(id)
                 }
             }
             Function(id, terms) => {
                 let terms = terms
                     .into_iter()
-                    .map(|term| term.replace_unification_terms(map))
+                    .map(|term| term.replace_vars(map))
                     .collect();
                 Function(id, terms)
             }
         }
     }
-
-    // TODO: 2023/07/06 to_str的なのを書く
 }
 
-impl std::fmt::Display for Term {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+// TODO: 2023/08/21 そもそもこれは必要なのか
+impl fmt::Display for Term {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use Term::*;
         match self {
             Var(id) => write!(f, "{id}"),
-            UnificationTerm((id1, id2)) => write!(f, "t({id1}, {id2})"),
             Function(name, terms) => {
                 write!(
                     f,
@@ -149,27 +109,16 @@ impl std::fmt::Display for Term {
                         .collect::<Vec<_>>()
                         .join(", ")
                 )
-                // write!(f, "{name}(")?;
-                // for (i, term) in terms.iter().enumerate() {
-                //     if i > 0 {
-                //         write!(f, ", ")?;
-                //     }
-                //     write!(f, "{}", term)?;
-                // }
-                // write!(f, ")")
             }
-            Dummy => write!(f, "_"),
         }
     }
 }
 
-impl Var {
-    fn get_fresh_var(self, old_vars: HashSet<&VarID>) -> Var {
-        let Var(id) = self;
-        if old_vars.contains(&id) {
-            Var(id + "'").get_fresh_var(old_vars)
-        } else {
-            Var(id)
-        }
+// TODO: 2023/08/21 型や所有権は要検討
+fn get_new_sig(sig: String, set: &HashSet<String>) -> String {
+    if set.contains(&sig) {
+        get_new_sig(sig + "'", set)
+    } else {
+        sig
     }
 }
