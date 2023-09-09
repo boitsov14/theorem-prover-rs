3b219c35149131df9be0ca8fb19ec71dd066f955
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-09-09 21:28:28 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod formula;
+mod parser;
+
+use crate::parser::parse;
+
 fn main() {
     println!("Hello, world!");
+    let s = "P and Q";
+    let Some((fml, inf)) = parse(s) else {
+        return;
+    };
+    println!("{}", fml.to_str_inf(&inf));
 }
