4268a75253a222cb2db348d9792222d01c539c7a
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-10-09 00:04:21 +0900

    impl tactic

--- src/prover.rs
+++ src/prover.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
 
-use crate::formula::{All, And, Exists, Formula, Iff, Implies, NamingInfo, Not, Or, Predicate};
+use crate::formula::{All, And, Exists, Formula, Iff, Implies, Not, Or, Predicate, FALSE, TRUE};
 
+/// Structured set of formulae
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 struct Formulae {
     predicate_set: HashSet<Predicate>,
@@ -14,16 +15,46 @@ struct Formulae {
     exists_set: HashSet<Exists>,
 }
 
+/// Sequent of structured formulae
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct Sequent {
     antecedent: Formulae,
     succedent: Formulae,
 }
 
+/// Sequent of formulae
 #[derive(Clone, Debug, Eq, PartialEq)]
 struct PlainSequent {
     antecedent: HashSet<Formula>,
     succedent: HashSet<Formula>,
+}
+
+#[derive(Debug)]
+enum AxiomTactic {
+    Axiom,
+    LFalse,
+    RTrue,
+}
+
+#[derive(Debug)]
+enum UnaryTactic {
+    LNot,
+    RNot,
+    LAnd,
+    ROr,
+    RImplies,
+    LIff,
+}
+
+#[derive(Debug)]
+enum BinaryTactic {
+    LImplies,
+    RIff,
+}
+
+enum NaryTactic {
+    RAnd,
+    LOr,
 }
 
 impl Sequent {
@@ -31,6 +62,24 @@ impl Sequent {
         Self {
             antecedent,
             succedent,
+        }
+    }
+    fn to_axiom(&self) -> Option<AxiomTactic> {
+        use AxiomTactic::*;
+        if self
+            .antecedent
+            .predicate_set
+            .intersection(&self.succedent.predicate_set)
+            .next()
+            .is_some()
+        {
+            Some(Axiom)
+        } else if self.antecedent.or_set.contains(&FALSE) {
+            Some(LFalse)
+        } else if self.succedent.and_set.contains(&TRUE) {
+            Some(RTrue)
+        } else {
+            None
         }
     }
 }
