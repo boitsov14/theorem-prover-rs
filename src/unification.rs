11da1ab3c5f23c216835ee3987d3570f1592aec1
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-03-08 02:41:37 +0900

    modify

--- src/unification.rs
+++ src/unification.rs
@@ -1,7 +1,8 @@
 use crate::formula::Term;
+use std::collections::HashMap;
 use Term::*;
 
-type Unifier = Vec<Option<Term>>;
+type Unifier = HashMap<usize, Term>;
 
 #[derive(Debug, PartialEq)]
 struct UnificationFailure;
@@ -16,7 +17,7 @@ impl Term {
                 if t.has_fv(*v, u) {
                     return Err(UnificationFailure);
                 }
-                u[*v] = Some(t.clone());
+                u.insert(*v, t.clone());
                 Ok(())
             }
             (Func(id1, args1), Func(id2, args2)) => {
@@ -34,7 +35,7 @@ impl Term {
     fn resolve<'a>(&'a self, u: &'a Unifier) -> &'a Self {
         match self {
             Var(v) => {
-                if let Some(t) = &u[*v] {
+                if let Some(t) = u.get(v) {
                     t.resolve(u)
                 } else {
                     self
@@ -60,43 +61,61 @@ mod tests {
     use std::collections::HashMap;
 
     #[test]
-    fn test_unify1() {
+    fn test_unify_suc1() {
         // x = f(y)
         let result = test_unify("x", "f(y)");
         // x ↦ f(y)
-        let expected = hashmap!["x".into() => Some("f(y)".into()), "y".into() => None];
+        let expected = hashmap!["x".into() => "f(y)".into()];
         assert_eq!(result, Ok(expected));
     }
 
     #[test]
-    fn test_unify2() {
+    fn test_unify_suc2() {
         // f(x, g(y)) = f(g(z), w)
         let result = test_unify("f(x, g(y))", "f(g(z), w)");
         // x ↦ g(z), w ↦ g(y)
-        let expected = hashmap!["x".into() => Some("g(z)".into()), "w".into() => Some("g(y)".into()), "z".into() => None, "y".into() => None];
+        let expected = hashmap!["x".into() => "g(z)".into(), "w".into() => "g(y)".into()];
         assert_eq!(result, Ok(expected));
     }
 
     #[test]
-    fn test_unify3() {
+    fn test_unify_suc3() {
         // f(x, y) = f(y, x)
         let result = test_unify("f(x, y)", "f(y, x)");
         // x ↦ y
-        let expected = hashmap!["x".into() => Some("y".into()), "y".into() => None];
+        let expected = hashmap!["x".into() => "y".into()];
         assert_eq!(result, Ok(expected));
     }
 
     #[test]
-    fn test_unify4() {
+    fn test_unify_suc4() {
         // f(x, g(x)) = f(y, z)
         let result = test_unify("f(x, g(x))", "f(y, z)");
         // x ↦ y, z ↦ g(x)
-        let expected = hashmap!["x".into() => Some("y".into()), "z".into() => Some("g(x)".into()), "y".into() => None];
+        let expected = hashmap!["x".into() => "y".into(), "z".into() => "g(x)".into()];
         assert_eq!(result, Ok(expected));
     }
 
     #[test]
-    fn test_unify5() {
+    fn test_unify_suc5() {
+        // f(x, y, z, x) = f(y, z, g(u), g(v))
+        let result = test_unify("f(x, y, z, x)", "f(y, z, g(u), g(v))");
+        // x ↦ y, y ↦ z, z ↦ g(u), u ↦ v
+        let expected = hashmap!["x".into() => "y".into(), "y".into() => "z".into(), "z".into() => "g(u)".into(), "u".into() => "v".into()];
+        assert_eq!(result, Ok(expected));
+    }
+
+    #[test]
+    fn test_unify_suc6() {
+        // f(x, y, z, x) = f(y, z, u, v)
+        let result = test_unify("f(x, y, z, x)", "f(y, z, u, v)");
+        // x ↦ y, y ↦ z, z ↦ u, u ↦ v
+        let expected = hashmap!["x".into() => "y".into(), "y".into() => "z".into(), "z".into() => "u".into(), "u".into() => "v".into()];
+        assert_eq!(result, Ok(expected));
+    }
+
+    #[test]
+    fn test_unify_fail1() {
         // f(x) = g(x)
         let result = test_unify("f(x)", "g(x)");
         // Fail
@@ -104,55 +123,60 @@ mod tests {
     }
 
     #[test]
-    fn test_unify6() {
-        // f(x) = f(g(x))
-        let result = test_unify("f(x)", "f(g(x))");
+    fn test_unify_fail2() {
+        // x = f(y, g(z, x))
+        let result = test_unify("x", "f(y, g(z, x))");
         // Fail
         assert_eq!(result, Err(UnificationFailure));
     }
 
     #[test]
-    fn test_unify7() {
+    fn test_unify_fail3() {
         // f(y, y) = f(g(x), x)
         let result = test_unify("f(y, y)", "f(g(x), x)");
         // Fail
         assert_eq!(result, Err(UnificationFailure));
     }
 
-    fn test_unify(s: &str, t: &str) -> Result<HashMap<String, Option<String>>, UnificationFailure> {
+    #[test]
+    fn test_unify_fail4() {
+        // f(x, y, z, x) = f(y, z, u, g(u))
+        let result = test_unify("f(x, y, z, x)", "f(y, z, u, g(u))");
+        // Fail
+        assert_eq!(result, Err(UnificationFailure));
+    }
+
+    fn test_unify(s: &str, t: &str) -> Result<HashMap<String, String>, UnificationFailure> {
         let fml_str = format!("P({s}, {t})");
         let (fml, entities) = parse(&fml_str).unwrap();
         let fml = fml.universal_quantify();
         let Formula::All(vs, mut p) = fml else {
             unreachable!()
         };
-        let mut free_var_idx = 0;
-        let mut free_var_info = vec![];
-        for v in vs {
-            p.subst(v, &Var(free_var_idx));
-            free_var_idx += 1;
-            free_var_info.push(v);
+        let mut new_id = entities.len();
+        let mut free_var_info = hashmap!();
+        for old_v in vs {
+            p.subst(old_v, &Var(new_id));
+            free_var_info.insert(new_id, old_v);
+            new_id += 1;
         }
         let Formula::Predicate(_, args) = *p else {
             unreachable!()
         };
         let t1 = args[0].clone();
         let t2 = args[1].clone();
-        let mut u = vec![None; free_var_idx];
+        let mut u = hashmap!();
         t1.unify(&t2, &mut u)?;
         let mut result = hashmap!();
-        for (new_v, t) in u.iter().enumerate() {
-            let old_v = free_var_info[new_v];
-            let old_v_str = Var(old_v).display(&entities).to_string();
-            if let Some(t) = t {
-                let mut t = t.clone();
-                for (new_v, old_v) in free_var_info.iter().enumerate() {
-                    t.subst(new_v, &Var(*old_v));
-                }
-                result.insert(old_v_str, Some(t.display(&entities).to_string()));
-            } else {
-                result.insert(old_v_str, None);
+        for (new_v, t) in u.iter_mut() {
+            for (new_v, old_v) in &free_var_info {
+                t.subst(*new_v, &Var(*old_v));
             }
+            let old_v = free_var_info[&new_v];
+            result.insert(
+                Var(old_v).display(&entities).to_string(),
+                t.display(&entities).to_string(),
+            );
         }
         Ok(result)
     }
