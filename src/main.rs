e9897ac8a4b3d9c66457b236f540c5aceffef799
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-04-29 20:37:20 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,19 @@ fn main() {
     // let s = "P or Q to Q or P";
     // let s = "¬(P ∧ Q) ↔ (¬P ∨ ¬Q)";
 
+    // let s = "all x P(x) to all y P(y)";
+    // let s = "ex x P(x) to ex y P(y)";
+    // let s = "all x P(x) to ex y P(y)";
+    // let s = "ex x P(x) to all y P(y)";
+    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(a))";
+    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(a)))";
+    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(f(f(f(f(f(f(f(f(a)))))))))))";
+    // let s = "∃x∀yP(x,y) → ∀y∃xP(x,y)";
+
+    // sort: 5274ms, un_sort: 1805ms -> 998ms
+    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(f(f(f(f(f(f(f(a))))))))))";
+    // u: {7: Func(3, [Func(3, [Func(4, [])])]), 9: Func(3, [Func(3, [Func(3, [Func(3, [Func(4, [])])])])]), 13: Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(4, [])])])])])])])])]), 10: Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(4, [])])])])])]), 8: Func(3, [Func(3, [Func(3, [Func(4, [])])])]), 12: Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(4, [])])])])])])])]), 6: Func(3, [Func(4, [])]), 5: Func(4, []), 11: Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(3, [Func(4, [])])])])])])])}
+
     example(s).unwrap();
     example_iltp_prop();
 }
