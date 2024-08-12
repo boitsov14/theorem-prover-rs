dc38d18b01484162c70e9dd093a0e07dbb1179ae
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-08-12 12:26:35 +0900

    wip

--- src/new_prover/lang.rs
+++ src/new_prover/lang.rs
@@ -1,4 +1,5 @@
 use crate::lang::Formula;
+use crate::new_prover::lang::Side::{Left, Right};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum Side {
@@ -24,4 +25,19 @@ struct FormulaExtended<'a> {
 struct SequentExtended<'a> {
     seq: Vec<FormulaExtended<'a>>,
     finished: bool,
+}
+
+impl Formula {
+    fn get_cost(&self, side: Side) -> Cost {
+        use Cost::*;
+        use Formula::*;
+        use Side::*;
+        match (self, side) {
+            (Pred(..), _) => Atom,
+            (And(_) | Ex(..), Left) | (Or(_) | To(..) | All(..), Right) | (Not(_), _) => Alpha,
+            (And(l), Right) | (Or(l), Left) => Beta(l.len()),
+            (To(..), Left) | (Iff(..), _) => Beta(2),
+            (All(..), Left) | (Ex(..), Right) => Quant,
+        }
+    }
 }
