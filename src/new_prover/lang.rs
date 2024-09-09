3fc28b7f1b364f66259a3a23dac799656b5e1286
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-09-09 22:44:10 +0900

    wip

--- src/new_prover/lang.rs
+++ src/new_prover/lang.rs
@@ -1,5 +1,4 @@
-use crate::lang::{Formula, Formula::*, FALSE, TRUE};
-use itertools::Itertools;
+use crate::lang::{Formula, Formula::*};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub(super) enum Side {
@@ -31,12 +30,6 @@ pub(super) struct SequentIdx {
     pub(super) atom: usize,
     pub(super) beta: usize,
     pub(super) end: usize,
-}
-
-// TODO: 2024/08/17 あとで消す
-struct SequentExtended<'a> {
-    seq: Vec<FormulaExtended<'a>>,
-    finished: bool,
 }
 
 impl Formula {
@@ -107,87 +100,5 @@ impl<'a> FormulaExtended<'a> {
             side,
             cost: fml.get_cost(side),
         }
-    }
-}
-
-// TODO: 2024/08/17 あとで消す
-impl<'a> SequentExtended<'a> {
-    fn new(ant: &'a [Formula], suc: &'a [Formula]) -> Self {
-        let mut seq = Self {
-            seq: Vec::with_capacity(ant.len() + suc.len()),
-            finished: false,
-        };
-        for fml in ant.iter().rev() {
-            seq.push(FormulaExtended::new(fml, Side::Left));
-        }
-        for fml in suc.iter().rev() {
-            seq.push(FormulaExtended::new(fml, Side::Right));
-        }
-        seq
-    }
-
-    fn push(&mut self, fml: FormulaExtended<'a>) {
-        if self
-            .seq
-            .iter()
-            .any(|p| p.fml == fml.fml && p.side != fml.side)
-        {
-            self.finished = true;
-        }
-
-        if (*fml.fml == TRUE && fml.side == Side::Right)
-            || (*fml.fml == FALSE && fml.side == Side::Left)
-        {
-            self.finished = true;
-        }
-
-        let i = self
-            .seq
-            .iter()
-            .rposition(|p| p.cost >= fml.cost)
-            .map_or(0, |x| x + 1);
-        self.seq.insert(i, fml);
-    }
-
-    fn pop(&mut self) -> Option<FormulaExtended<'a>> {
-        use Cost::*;
-        use Side::*;
-        let fml = self.seq.last()?;
-        match fml.cost {
-            Quant | Redundant | Atom => {
-                return None;
-            }
-            _ => {}
-        }
-        let mut fml = self.seq.pop()?;
-        match (fml.fml, fml.side) {
-            (And(l), Right) => {
-                if self
-                    .seq
-                    .iter()
-                    .any(|p| p.side == Right && l.iter().contains(p.fml))
-                {
-                    fml.cost = Redundant;
-                    self.push(fml);
-                    return self.pop();
-                }
-            }
-            (Or(l), Left) => {
-                if self
-                    .seq
-                    .iter()
-                    .any(|p| p.side == Left && l.iter().contains(p.fml))
-                {
-                    fml.cost = Redundant;
-                    self.push(fml);
-                    return self.pop();
-                }
-            }
-            (To(p, q), Left) => {}
-            (Iff(p, q), Left) => {}
-            (Iff(p, q), Right) => {}
-            _ => {}
-        }
-        Some(fml)
     }
 }
