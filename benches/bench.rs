83b6d8c43a9b6975ec4c49c9082a54f7e482a58c
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-02-16 21:28:44 +0900

    wip

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,3 @@
-/*
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use mimalloc::MiMalloc;
 use theorem_prover_rs::{prover::prover::prove_prop, read_file_and_parse};
@@ -24,4 +23,3 @@ fn hard_props(c: &mut Criterion) {
 
 criterion_group!(benches, hard_props,);
 criterion_main!(benches);
-*/
