3eab62686db020809f9cc825764a38ffd28c2bf9
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-02-23 21:44:09 +0900

    wip

--- src/prover.rs
+++ src/prover.rs
@@ -68,4 +68,11 @@ mod bench {
         let (seq, names) = parse_nth("examples/hard-props.txt", &arena, n).unwrap();
         bencher.bench_local(|| prove_prop(seq.clone(), &names));
     }
+
+    #[divan::bench(args = [0,1,2,3])]
+    fn bench_ebproof(bencher: Bencher, n: usize) {
+        let arena = Arena::new();
+        let (seq, names) = parse_nth("examples/large-latex.txt", &arena, n).unwrap();
+        bencher.bench_local(|| ebproof(seq.clone(), &names));
+    }
 }
