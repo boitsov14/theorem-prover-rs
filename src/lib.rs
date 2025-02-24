6d31c5a9fbce4b3b5678470fd90eac7638cc405d
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2025-02-25 00:47:58 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@ mod parser;
 mod prover;
 
 use clap::Parser;
+use itertools::Itertools;
 use mimalloc::MiMalloc;
 use prover::prove;
+use std::{fs, path::PathBuf};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -42,23 +44,12 @@ pub fn main_prover() {
         println!("Using forest format");
     }
 
-    let s = "P and Q to Q and P";
-    // 0.80520004 ms to prove
-    // 547.71094 ms to print to file -> 10ms
-    // 1.6MB
-    // let s = "(((((((((p6↔p7)↔p8)↔p9)↔p10)↔p11)↔p12)↔p13)↔p14)↔(p14↔(p13↔(p12↔(p11↔(p10↔(p9↔(p8↔(p7↔p6)))))))))";
-    // let s = "P or Q to Q or P";
-    // let s = "¬(P ∧ Q) ↔ (¬P ∨ ¬Q)";
-    // let s = "all x P(x) to all y P(y)";
-    // let s = "ex x P(x) to ex y P(y)";
-    // let s = "all x P(x) to ex y P(y)";
-    // let s = "ex x P(x) to all y P(y)";
-    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(a))";
-    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(a)))";
-    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(f(f(f(f(f(f(f(f(a)))))))))))";
-    // let s = "∃x∀yP(x,y) → ∀y∃xP(x,y)";
-    // sort: 5274ms, un_sort: 1805ms -> 998ms
-    // let s = "P(a) to all x(P(x) → P(f(x))) to P(f(f(f(f(f(f(f(f(f(a))))))))))";
+    // read formula from file
+    let s = fs::read_to_string(PathBuf::from(&config.out).join("formula.txt"))
+        .expect("Failed to read formula.txt")
+        .lines()
+        .filter(|l| !l.trim_start().starts_with('#'))
+        .join("\n");
 
-    prove(s).unwrap();
+    prove(&s).unwrap();
 }
